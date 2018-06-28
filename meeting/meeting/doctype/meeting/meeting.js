84e7271eb4d470ccae9e86b56eb8a20925fda3bb
asutosh05 <asutosh407@gmail.com>
2018-06-29 00:20:02 +0530

    all doc type created and some basic validation

--- meeting/meeting/doctype/meeting/meeting.js
+++ meeting/meeting/doctype/meeting/meeting.js
@@ -1,8 +1,23 @@
 // Copyright (c) 2018, Asutosh and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Meeting', {
-	refresh: function(frm) {
-
+frappe.ui.form.on('Meeting Attendee', {
+	attendee: function(frm,cdt,cdn) {
+		var attendee=frappe.model.get_doc(cdt,cdn);
+		if(attendee.attendee){
+			frm.call({
+				method:"meeting.meeting.doctype.meeting.meeting.get_full_name",
+				args:{
+					attendee:attendee.attendee
+				},
+				callback:function(r){
+					frappe.model.set_value(cdt,cdn,"full_name",r.message);
+				}
+			});
+		}else{
+			//To clear full name if attendess not present
+			frappe.model.set_value(cdt,cdn,"full_name",null);
+		}
 	}
+
 });
--- meeting/meeting/doctype/meeting/meeting.py
+++ meeting/meeting/doctype/meeting/meeting.py
@@ -4,7 +4,22 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe import _
 from frappe.model.document import Document
 
 class Meeting(Document):
-	pass
+	def validate(self):
+		"""set missing names"""
+		found=[]
+		for attendee in self.attendees:
+			if not attendee.full_name:
+				attendee.full_name=get_full_name(attendee.attendee)
+			if attendee in found:
+				frappe.throw(_("Attendee {0} found more the one").format(attendee.attendee))
+			found.append(attendee.attendee)
+
+#To whitelist the function:This muntion can be any where
+@frappe.whitelist()
+def get_full_name(attendee):
+	user=frappe.get_doc("User",attendee)
+	return " ".join(filter(None,[user.first_name,user.middle_name,user.last_name]))
